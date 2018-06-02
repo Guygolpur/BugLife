28a012501bccc2243f01f4bc63508b9d73b23788
Guygolpur <guygolpur@se.shenkar.ac.il>
2018-06-02 18:04:15 +0300

    BugLife-Guy Golpur Step3 done

--- includes/main.js
+++ includes/main.js
@@ -1,4 +1,5 @@
 $("document").ready(function() {
+
 $(".share").on('click', function(e) {
   $(".fab").removeClass("no");
   if(e.target != this) return;
@@ -13,6 +14,7 @@ $(".share").on('click', function(e) {
 });
 	});
 
+/*-----------buttons-Step3-Mobile-------------*/
 function ring()
 {
 	document.getElementById("bellStep3").classList.toggle("active")
@@ -21,4 +23,34 @@ function ring()
 function light()
 {
 	document.getElementById("lightOffStep3").classList.toggle("active")
+}
+
+
+
+
+/*-----------Pop-Up-Step3-Desktop-------------*/
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
