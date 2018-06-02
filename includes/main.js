053583deee6d327b960289d28eddea80a7197cab
Guygolpur <guygolpur@se.shenkar.ac.il>
2018-06-02 13:45:49 +0300

    BugsLife-Guy Golpur share commit

--- includes/main.js
+++ includes/main.js
@@ -1,3 +1,12 @@
+$("document").ready(function() {
+$(".share").on('click', function(e) {
+  $(".fab").removeClass("no");
+  if(e.target != this) return;
+  $('.share, .fab').toggleClass("active");
+});
+	
+	});
+
 function ring()
 {
 	document.getElementById("bellStep3").classList.toggle("active")
